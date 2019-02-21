0ba4ff8053fc864bbf82317d8d993b1cf3462937
Gena Jidkiy <42508019+TJesT@users.noreply.github.com>
2019-02-22 00:15:00 +0700

    unnecessary Ruler class initial commit

---
+++ Ruler.py
@@ -0,0 +1,13 @@
+class Ruler:
+    """
+    Необязательный класс
+    создан на будущее к выбору разных персов
+    """
+    def __init__(self):
+        self.isDead = False
+        
+        self.hp = 30000
+        self.gold = 10000
+        self.faith = 0
+        self.humidity = 0
+        self.territory = 50
