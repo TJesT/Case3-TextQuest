1d0210dffa346f27ca0af966fdd656284b4ae0be
Gena Jidkiy <42508019+TJesT@users.noreply.github.com>
2019-02-28 20:06:31 +0700

    some edits

--- EnemyState.py
+++ EnemyState.py
@@ -1,15 +1,18 @@
+from State import *
 import random
 
 RELIGIONS = ["Сатанизм", "Христианство", "Коммунизм", "Атеизм", "Киберспорт"]
 NAMES = ["Адольф", "Омарон", "Игилий", "Авкадий", "Дима", "Тимур"]
 
-class EnemyState:
+class EnemyState(State):
 	def __init__(self, difficulty):
+		State.count_of_states += 1
+
 		self.hp = 10000*difficulty
 		self.inWar = False
 		self.ruler_is_sick = False
 
-		self.territory = random.randint(50, 200)
+		self.territory = random.randint(25*difficulty, 100 * difficulty)
 
 		self.lieges = 40 * (200/self.territory)
 		self.army = 16 * (200/self.territory)
@@ -20,11 +23,12 @@ class EnemyState:
 		self.RELIGION = RELIGIONS[random.randint(0, len(RELIGIONS) - 1)]
 		self.NAME = NAMES[random.randint(0, len(NAMES) - 1)]
 
-  def declareWar(self, state):
-    self.inWar = True
-    state.inWar = True
-
 	def weLost(self, state):
 		state.lieges += self.lieges
 		state.harvest += self.harvest
-    state.inWar = False
+		this.defeated = True
+
+	def toString(self):
+		return (" ".join((self.hp, self.inWar, self.ruler_is_sick, self.territory, \
+		self.lieges, self.army,	self.smute,	self.tax, self.harvest,	self.RELIGION, \
+		self.NAME)))
