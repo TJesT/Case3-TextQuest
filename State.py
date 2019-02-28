f3e2021a5511aab381c95fe340a5e675aae39bc0
Gena Jidkiy <42508019+TJesT@users.noreply.github.com>
2019-02-28 22:26:55 +0700

    __str__ overload

--- State.py
+++ State.py
@@ -9,3 +9,8 @@ class State:
 		print("Подданые:", self.lieges)
 		print("Армия:", self.army)
 		print("Территории:", self.territory)
+
+	def __str__(self):
+		return (" ".join(map(str, (self.hp, self.inWar, self.ruler_is_sick, self.territory, \
+		self.lieges, self.army,	self.smute,	self.tax, self.harvest,	self.RELIGION, \
+		self.NAME))))
