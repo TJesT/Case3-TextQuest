a5c0cad0f73ed880d29bf63bedc723c7da962bda
Gena Jidkiy <42508019+TJesT@users.noreply.github.com>
2019-02-28 23:12:02 +0700

    Update Game.py

--- Game.py
+++ Game.py
@@ -1,20 +1,27 @@
+from math import randint
+from EnemyState import *
+from PlayerState import *
+
 class Game:
-	def start():
-		state=State(Ruler())
-		enemies = []
-		enemies.append()
+	def start(self):
+		state = PlayerState(Ruler())
+		enemies = [EnemyState(1), EnemyState(1), EnemyState(1)] 
 		
-		while(True):
-			if state.hp<=0 or state.territory+state.seeded<=0 or state.smute>=100%:
-				break
-			state.seed(state.harvest+int(input("Skolko seyat?"))
-			state.giveOut(int(input("Skolko kushac?"))
-			state.sellHarvest(int(input()))
-			state.recruit(int(input()))
-			state.collect()
+		while (True):
+			if state.hp <= 0 or state.territory + state.seeded <= 0 or state.smute >= 100 :
+				print("u_lost")
+				state.seed(state.harvest + int(input("Skolko seyat? - ")))
+				state.giveOut(int(input("Skolko kushac? - ")))
+				state.sellHarvest(int(input()))
+				state.recruit(int(input())) 
+				state.collect() 
 			if state.ruler_is_sick:
-				state.hp-=100
-			event().relief()
-			event().get_sick()
-			event().meteor()
-			event().bless()
+				state.hp -= 100
+			if randint(0, 10) == 0:
+				event().relief()
+			elif randint(0, 30) == 0:
+				event().get_sick(state)
+			event().meteor(state)
+			event().rats(state)
+			event().disorder(state)
+			event().prosper(state)
